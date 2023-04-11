c5a54f4edac0f7ca606464f1f1c27cc860436cfd
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-04-11 14:07:30 +0200

    added update_key_values func

--- src/graph_functions.py
+++ src/graph_functions.py
@@ -10,8 +10,68 @@ import momepy
 import osmnx as ox
 
 
-def clean_col_names(df):
+def update_key_values(bicycle_graph):
+    """
+    Make sure that no edges exist with key=1 in the multiindex, without a corresponding key=0 (this can occur due to how the bicycle graph is created).
+    If key=1 edges without a parallel corresponding edges are found, the key value is set to 0.
+    If this is not done, some OSMnx function will not function correctly.
 
+    Arguments:
+        bicycle_graph (networkx/osmnx graph): graph to check for edges with wrong key-value.
+
+    Returns:
+        updated_bicycle_graph (networkx/osmnx graph): updated graph with no key=1 edges without a corresponding key=0 edge
+    """
+
+    bicycle_edges = ox.graph_to_gdfs(bicycle_graph, nodes=False)
+
+    # Unfreeze graph
+    bicycle_graph_unfrozen = bicycle_graph.copy()
+
+    try:
+        # Find edges that have only key 1, but no key 0
+        key1edges = bicycle_edges.loc[:, :, 1].index
+        edges_to_key0 = []
+        for edge in key1edges:
+            if 0 not in bicycle_edges.loc[edge].index.get_level_values(0):
+                edges_to_key0.append(edge)
+
+        # For each of these edges,
+        for e in edges_to_key0:
+            # Create an edge copy with key 0 (but same attributes)
+            # Then delete the key 1 edge
+            myedgeattrs = bicycle_graph_unfrozen.edges[e[0], e[1], 1]
+            bicycle_graph_unfrozen.add_edge(e[0], e[1], 0)
+            bicycle_graph_unfrozen.edges[e[0], e[1], 0].update(myedgeattrs)
+            bicycle_graph_unfrozen.remove_edge(e[0], e[1], 1)
+
+        # Derive gdfs once more, from updated graph
+        bicycle_edges = ox.graph_to_gdfs(bicycle_graph_unfrozen, nodes=False)
+
+        # Run once more for assertion:
+
+        # Find edges that have only key 1, but no key 0
+        key1edges = bicycle_edges.loc[:, :, 1].index
+        edges_to_key0 = []
+        for edge in key1edges:
+            if 0 not in bicycle_edges.loc[edge].index.get_level_values(0):
+                edges_to_key0.append(edge)
+        assert len(edges_to_key0) == 0
+
+        # Set the bicycle graph to be the unfrozen & updated one:
+        del bicycle_graph
+        updated_bicycle_graph = bicycle_graph_unfrozen
+        del bicycle_graph_unfrozen
+
+        return updated_bicycle_graph
+
+    except KeyError:
+        print("No update necessary. Returning original bicycle graph.")
+
+        return bicycle_graph
+
+
+def clean_col_names(df):
     """
     Remove upper-case letters and : from data with OSM tags
     Special characters like ':' can for example break with pd.query function
@@ -35,7 +95,6 @@ def clean_col_names(df):
 
 
 def unzip_linestrings(org_gdf, edge_id_col):
-
     """
     Splits lines into their smallest possible line geometry, so each line only is defined by the start and end coordinate.
     Used to convert reference data to a similar data structure as used by osnmnx
@@ -80,7 +139,6 @@ def unzip_linestrings(org_gdf, edge_id_col):
 
 
 def create_osmnx_graph(gdf):
-
     """
     Function for  converting a geodataframe with LineStrings to a NetworkX graph object (MultiDiGraph), which follows the data structure required by OSMnx.
     (I.e. Nodes indexed by osmid, nodes contain columns with x and y coordinates, edges is multiindexed by u, v, key).
@@ -144,7 +202,6 @@ def create_osmnx_graph(gdf):
 
 
 def find_parallel_edges(edges):
-
     """
     Check for parallel edges in a pandas DataFrame with edges, including columns u with start node index and v with end node index.
     If two edges have the same u-v pair, the column 'key' is updated to ensure that the u-v-key combination can uniquely identify an edge.
@@ -165,7 +222,6 @@ def find_parallel_edges(edges):
     k = 1
 
     while len(edges[edges.duplicated(subset=["u", "v", "key"])]) > 0:
-
         k += 1
 
         parallel = edges[edges.duplicated(subset=["u", "v", "key"])]
@@ -180,7 +236,6 @@ def find_parallel_edges(edges):
 
 
 def create_node_index(x, index_length):
-
     """
     Create unique id or index value of specific length based on another shorter column
 
